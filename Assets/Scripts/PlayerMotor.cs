dc60dacf3626e392233fa525f582494c1bf6a6d9
Iliyan <iliyan.yankov@channelcapital.io>
2022-10-30 16:47:02 +0000

    basic player movement added

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+  private CharacterController controller;
+  private Vector3 playerVelocity;
+  private bool isGrounded;
+  public float speed = 5f;
+  public float gravity = -9.8f;
+  public float jumpVelocity = 1.4f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    controller = GetComponent<CharacterController>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    isGrounded = controller.isGrounded;
+  }
+
+  // Receive the input from the InputManager.cs and apply them to  the character controller
+  public void processMove(Vector2 input)
+  {
+    Vector3 moveDirection = Vector3.zero;
+    moveDirection.x = input.x;
+    moveDirection.z = input.y;
+    controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+    playerVelocity.y += gravity * Time.deltaTime;
+    if (isGrounded && playerVelocity.y < 0) playerVelocity.y = -2;
+    controller.Move(playerVelocity * Time.deltaTime);
+  }
+
+  public void Jump()
+  {
+
+    if (isGrounded)
+    {
+      playerVelocity.y = Mathf.Sqrt(jumpVelocity * -3.0f * gravity);
+    }
+  }
+
+}
